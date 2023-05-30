e042e48446926b1f2b6471fa9de3f9ca7d5a1080
JyHwa0702 <livm211@daum.net>
2023-05-30 21:38:44 +0900

    폴더 이름바꿈

--- LolData/src/main/java/JyHwa/LolData/Controller/LeagueAPIController.java
+++ LolData/src/main/java/JyHwa/LolData/Controller/LeagueAPIController.java
@@ -3,11 +3,8 @@ package JyHwa.LolData.Controller;
 import JyHwa.LolData.Dto.LeagueEntryDto.LeagueEntryDto;
 import JyHwa.LolData.Service.LeagueService;
 import lombok.RequiredArgsConstructor;
-import lombok.Value;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
