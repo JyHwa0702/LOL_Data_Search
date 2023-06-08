afafa21c213c7065eee6ff9972966a69352cced5
JyHwa0702 <livm211@daum.net>
2023-06-08 18:25:22 +0900

    파일정리

--- LolData/src/main/java/JyHwa/LolData/Dto/MatchDto/BanDto.java
+++
@@ -1,9 +0,0 @@
-package JyHwa.LolData.Dto.MatchDto;
-
-import lombok.Data;
-
-@Data
-public class BanDto {
-    private int championId;
-    private int pickTurn;
-}
--- LolData/src/main/java/JyHwa/LolData/Dto/MatchDto/ObjectiveDto.java
+++
@@ -1,9 +0,0 @@
-package JyHwa.LolData.Dto.MatchDto;
-
-import lombok.Data;
-
-@Data
-public class ObjectiveDto {
-    private boolean first;
-    private int kills;
-}
