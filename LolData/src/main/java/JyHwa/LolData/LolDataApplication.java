bfbbeee8f0ac76f610d620b178f54a3b9f541be8
JyHwa0702 <livm211@daum.net>
2023-06-08 17:31:49 +0900

    //dd

--- LolData/src/main/java/JyHwa/LolData/LolDataApplication.java
+++ LolData/src/main/java/JyHwa/LolData/LolDataApplication.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @SpringBootApplication
 //@EnableJpaRepositories(basePackages = "JyHwa.LolData.Repository")
 public class LolDataApplication {
+//	dd
 
 	public static void main(String[] args) {
 		SpringApplication.run(LolDataApplication.class, args);
